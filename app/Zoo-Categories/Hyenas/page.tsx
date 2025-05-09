01e2473129b3c27355ccf9e98b2bfec678c3a68c
Ahmed Bicir <ahmedhassanbicir.aa98@example.com>
2025-05-09 09:40:35 +0300

    updated Zoo- ctegories

--- app/Zoo-Categories/Hyenas/page.tsx
+++ app/Zoo-Categories/Hyenas/page.tsx
@@ -1,4 +1,4 @@
-export const Hyenas = () => (
+ const Hyenas = () => (
     <section className="p-8">
       <h2 className="text-4xl font-bold mb-4">Hyenas</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
